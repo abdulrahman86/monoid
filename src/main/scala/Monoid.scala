466f0b6906d1eddceb6f1bb0b1f6157dca086fc2
Abdul Sattar <asattar@exchangesolutions.com>
2016-10-13 14:39:09 -0400

    more monoid commit

--- src/main/scala/Monoid.scala
+++ src/main/scala/Monoid.scala
@@ -50,15 +50,28 @@ object Monoid {
     override def zero: Option[_] = None
   }
 
-  def endoMonoid[A] : Monoid[A => A] = new Monoid[(A) => A] {
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[(A) => A] {
 
     override def op(a1: (A) => A, a2: (A) => A): (A) => A = a1.andThen(a2)
 
     override def zero: (A) => A = x => x
   }
 
-  def concatenate[A: Monoid](in: List[A])(implicit monoid: Monoid[A]) : A =
+  def concatenate[A: Monoid](in: List[A])(implicit monoid: Monoid[A]): A =
     in.foldRight(monoid.zero)((a1, a2) => monoid.op(a1, a2))
 
+  def foldMap[A, B](as: List[A])(f: A => B)(implicit monoid: Monoid[B]): B = concatenate(as.map(f))
 
+  def foldMapV[A, B](v: IndexedSeq[A])(f: A => B)(implicit m: Monoid[B]): B = {
+
+    if (v.length > 1) {
+
+      val (a, b) = v.splitAt(v.length / 2);
+
+      m.op(foldMapV(a)(f), foldMapV(b)(f))
+    }
+
+    v.lastOption.map(f) getOrElse(m.zero)
+
+  }
 }
